02f102d0b018a734a8aa0569788fcfd0f95316b6
Johnny Valles <vallesja@uci.edu>
2020-06-12 16:52:55 -0700

    Fix navbar bg color on scroll

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
+$(function () {
+    $(document).scroll(function () {
+        let $nav = $("#navbar-container");
+        $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+    });
+});
+
 particlesJS.load("particles-js", 'particles.json');
